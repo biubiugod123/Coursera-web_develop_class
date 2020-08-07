1337ad43d1c119a6ed49bbb0cb2a3600167736bd
biubiu <biubiugod@gmail.com>
2020-08-07 03:53:43 -0400

    JAVASCRIPT

--- module4/js/script.js
+++ module4/js/script.js
@@ -1,2 +1,8 @@
-var x;
-console.log(x);
+
+var company = new Object();
+company.name = "facebook";
+company.ceo = new Object();
+company.ceo.firstName = "Jiantang";
+company.ceo.favColor = "black";
+console.log(company);
+console.log("Company ceo name is:" + company.ceo.firstName` );
